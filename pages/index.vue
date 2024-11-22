2dbec55a33057fc1cf1c56a7281776092fb058ab
Vanechkabublik <leonarov@vk.com>
2024-11-22 18:44:26 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="example">
+      <h1>Привет, Sass!</h1>
+    </div>
+  </template>
+  
+  <script setup>
+  // ваш JavaScript или TypeScript код
+  </script>
+  
+  <style lang="scss" scoped>
+  h1 {
+    color: red;
+  }
+  </style>
+  
